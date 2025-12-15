6ffc6953728d44dfda41b69f3392d95236bde7b2
JY8752 <j.y.8752@gmail.com>
2025-12-16 01:48:13 +0900

    storeの実装

---
+++ src/store.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+use tokio::sync::RwLock;
+
+pub type ID = String;
+pub struct Store<T>(Arc<RwLock<HashMap<ID, T>>>);
+
+impl<T> Default for Store<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T> Store<T> {
+    pub fn new() -> Self {
+        Self(Arc::new(RwLock::new(HashMap::new())))
+    }
+
+    pub async fn write(&self, id: &ID, value: T) {
+        self.0.write().await.insert(id.clone(), value);
+    }
+}
+
+impl<T: Clone> Store<T> {
+    pub async fn read(&self, id: &ID) -> Option<T> {
+        self.0.read().await.get(id).cloned()
+    }
+}
--- src/user.rs
+++ src/user.rs
@@ -1,9 +1,12 @@
 use tonic::{Request, Response, Status};
+use uuid::Uuid;
 
 use proto::v1::{
     CreateUserRequest, CreateUserResponse, GetUserRequest, GetUserResponse,
     user_service_server::UserServiceServer,
 };
+
+use crate::store::Store;
 
 mod proto {
     pub mod v1 {
@@ -11,8 +14,32 @@ mod proto {
     }
 }
 
-#[derive(Debug, Default)]
-pub struct UserService {}
+#[derive(Debug, Clone)]
+struct User {
+    id: String,
+    name: String,
+    email: String,
+}
+
+type UserStore = Store<User>;
+
+pub struct UserService {
+    store: UserStore,
+}
+
+impl UserService {
+    pub fn new() -> Self {
+        Self {
+            store: Store::new(),
+        }
+    }
+}
+
+impl Default for UserService {
+    fn default() -> Self {
+        Self::new()
+    }
+}
 
 #[tonic::async_trait]
 impl proto::v1::user_service_server::UserService for UserService {
@@ -20,23 +47,41 @@ impl proto::v1::user_service_server::UserService for UserService {
         &self,
         request: Request<CreateUserRequest>,
     ) -> Result<Response<CreateUserResponse>, Status> {
-        Ok(Response::new(CreateUserResponse {
-            id: "1".to_string(),
-        }))
+        let req = request.into_inner();
+        let id = Uuid::new_v4().to_string();
+
+        self.store
+            .write(
+                &id,
+                User {
+                    id: id.clone(),
+                    name: req.name,
+                    email: req.email,
+                },
+            )
+            .await;
+
+        Ok(Response::new(CreateUserResponse { id }))
     }
 
     async fn get_user(
         &self,
         request: Request<GetUserRequest>,
     ) -> Result<Response<GetUserResponse>, Status> {
-        Ok(Response::new(GetUserResponse {
-            id: "1".to_string(),
-            name: "John Doe".to_string(),
-            email: "".to_string(),
-        }))
+        let req = request.into_inner();
+
+        if let Some(user) = self.store.read(&req.id).await {
+            Ok(Response::new(GetUserResponse {
+                id: user.id.clone(),
+                name: user.name.clone(),
+                email: user.email.clone(),
+            }))
+        } else {
+            Err(Status::not_found(format!("User not found: {}", req.id)))
+        }
     }
 }
 
 pub fn get_service() -> UserServiceServer<UserService> {
-    UserServiceServer::new(UserService::default())
+    UserServiceServer::new(UserService::new())
 }
